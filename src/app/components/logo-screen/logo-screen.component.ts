5b640f14aa0ae3ff92a089c428c9590381300ba6
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-02-14 11:04:34 -0300

    update

--- src/app/components/logo-screen/logo-screen.component.ts
+++ src/app/components/logo-screen/logo-screen.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-logo-screen',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './logo-screen.component.html',
   styleUrl: './logo-screen.component.scss',
 })
 export class LogoScreenComponent {
-  constructor() {
+  @ViewChild('container') containerImg!: ElementRef;
+  constructor() {}
+  scrollToImage(imagemId: string) {
+    console.log(this.containerImg);
   }
-
 }
