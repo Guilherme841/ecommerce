4afc0291c907840c7fa7cb76cb14ef19099c02f2
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-02-14 20:52:55 -0300

    update

--- src/app/components/logo-screen/logo-screen.component.ts
+++ src/app/components/logo-screen/logo-screen.component.ts
@@ -8,12 +8,23 @@ import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
   styleUrl: './logo-screen.component.scss',
 })
 export class LogoScreenComponent {
-  @ViewChildren('containerImg') images!: QueryList<ElementRef>;
+  @ViewChildren('images') images!: QueryList<ElementRef>;
   constructor() {}
+  i: number = 0;
   next() {
-    // this.img2.nativeElement.scrollIntoView();
-    console.log(
-      this.images.forEach((image) => console.log(image.nativeElement))
-    );
+    if (this.i >= 2) {
+      return;
+    } else {
+      this.i++;
+      this.images.get(this.i)?.nativeElement.scrollIntoView();
+    }
+  }
+  back() {
+    if (this.i <= 0) {
+      return;
+    } else {
+      this.i--;
+      this.images.get(this.i)?.nativeElement.scrollIntoView();
+    }
   }
 }
