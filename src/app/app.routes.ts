4fbd01aa4addd02dd2b7f0c148876bf7f6919969
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-02-15 19:23:42 -0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { LogoScreenComponent } from './components/logo-screen/logo-screen.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LogoScreenComponent,
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
   },
 ];
--- src/app/components/logo-screen/logo-screen.component.ts
+++ src/app/components/logo-screen/logo-screen.component.ts
@@ -1,21 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-logo-screen',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './logo-screen.component.html',
   styleUrl: './logo-screen.component.scss',
 })
 export class LogoScreenComponent {
   @ViewChildren('slides') slides!: QueryList<ElementRef>;
-  constructor() {}
+  constructor(private _router: Router) {}
   i: number = 0;
   activated: boolean = true;
   next() {
     this.activated = !this.activated;
     if (this.i >= 2) {
+      this._router.navigate(['login']);
       return;
     } else {
       this.i++;
