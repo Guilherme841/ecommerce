0a9a13a46a1a8fad5d81c3356859bf477970f409
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-02-17 11:52:37 -0300

    add sign-up route and generate component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LogoScreenComponent } from './components/logo-screen/logo-screen.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,13 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./components/login/login.component').then(
         (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: 'sign-up',
+    loadComponent: () =>
+      import('./components/sign-up/sign-up.component').then(
+        (m) => m.SignUpComponent
       ),
   },
 ];
