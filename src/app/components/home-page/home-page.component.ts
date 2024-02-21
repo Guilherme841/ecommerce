f8ca1d6cad034cd65d79375a452af715adb63f91
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-02-20 22:34:27 -0300

    update

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoupeIconComponent } from '../../svg-components/loupe-icon/loupe-icon.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoupeIconComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
