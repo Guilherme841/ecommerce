1c0adb177b9a6ebd67e09872c239e7a308e4ec98
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2024-02-21 15:40:51 -0300

    update

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { LoupeIconComponent } from '../../svg-components/loupe-icon/loupe-icon.component';
+import { SortIconComponent } from '../../svg-components/sort-icon/sort-icon.component';
+import { FilterIconComponent } from '../../svg-components/filter-icon/filter-icon.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterOutlet,LoupeIconComponent],
+  imports: [
+    RouterOutlet,
+    LoupeIconComponent,
+    SortIconComponent,
+    FilterIconComponent,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
---
+++ src/app/svg-components/category-icons/beauty.component copy 3.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-beauty-icon',
+    standalone: true,
+    imports: [],
+    template: ``
+})
+export class AppBeautyComponent {} 
